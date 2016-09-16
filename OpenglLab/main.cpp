6bc079dc8dac205760c5ffdf82f0a51ebd015327
Yu Fang <1497272646@qq.com>
2016-09-16 15:55:04 +0800

    Add: add GLM library into project

--- OpenglLab/main.cpp
+++ OpenglLab/main.cpp
@@ -4,6 +4,8 @@
 #define  GLEW_STATIC
 #include <GL/glew.h>
 #include <glfw/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 GLuint vertexArrayID;
 GLuint vertexBuffer;
@@ -32,6 +34,19 @@ void prepare() {
 }
 
 void render() {
+	// Projection matrix : 45бу Field of View, 4:3 ratio, display range : 0.1 unit  100 units
+	glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+	// Camera matrix
+	glm::mat4 View = glm::lookAt(
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
+		glm::vec3(0, 0, 0), // and looks at the origin
+		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	// Model matrix : an identity matrix (model will be at the origin)
+	glm::mat4 Model = glm::mat4(1.0f);  // Changes for each model !
+										// Our ModelViewProjection : multiplication of our 3 matrices
+	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
     glVertexAttribPointer(
